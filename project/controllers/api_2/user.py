f82e1bed747a0dd50550c8f455ba03af8dcb9be6
VooDooBlooD <mdan.hagh@gmail.com>
2016-03-10 04:46:16 +0330

    add get_user endpoint

--- project/controllers/api_2/user.py
+++ project/controllers/api_2/user.py
@@ -5,7 +5,12 @@ from project import app
 from project.utils.validators import api_validate_schema
 
 
-@app.api_route('/user/', methods=['GET', 'POST'])
+@app.api_route('/user/')
+def get_user():
+	return '2', 200
+
+
+@app.api_route('/user/', methods=['POST'])
 @api_validate_schema('user.signup_schema')
 def signup():
 	return '2', 200
